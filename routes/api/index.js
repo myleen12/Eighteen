4f3dcb2cee6342ed370d2dbcf6b441d1938d4e20
myleen12 <myleen.santana12@icloud.com>
2023-05-07 22:34:50 -0700

    added routes to index.js file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
+const routes= require('./routes/index.js')
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
+router.use('/routes', index.js)
 
 module.exports = router;
